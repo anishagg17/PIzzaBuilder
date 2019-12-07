e9cf3324e4f9016c1cf61b26d9560578a7a3d4bf
Anish Aggarwal <anish17122000@gmail.com>
2019-12-07 23:05:40 +0530

    Updates

---
+++ precache-manifest.1b760f3991d23c89ce574f28758a1edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f282a0c5cd7699fdc1241e922b12fabd",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "a4d92a67d889cc927581",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3373b16e05998d759fe8",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/css/main.5f7fb0e7.chunk.css"
+  },
+  {
+    "revision": "a4d92a67d889cc927581",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/2.3335a204.chunk.js"
+  },
+  {
+    "revision": "3373b16e05998d759fe8",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/main.0347376b.chunk.js"
+  },
+  {
+    "revision": "6d9c4206777254c82bf3",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/runtime~main.bc0f63e2.js"
+  }
+]);
